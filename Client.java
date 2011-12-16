e03ccebc7aab807f670aaace52098b95453a8742
Nilo Redini <n.redini@gmail.com>
2011-12-16 19:53:53 +0100

    Added local password storage using SHA1 and salt, menus and registration/login

--- Client.java
+++ Client.java
@@ -20,21 +20,57 @@ public class Client {
 
     public static void main (String args[]){
         User user;
-        String str;
+        Menu menu= new Menu();
+        SecureLogin login = new SecureLogin();
+        String str = new String();
+        String []cred = new String[2]; /*cred[0] is the user name, cred[2] is the password*/
         int port;
+        int ch;
         ClientThread ct,cc;
         BufferedReader stdIn = new BufferedReader ( new InputStreamReader (System.in));
         try{
             port = Integer.parseInt(args[0]);
-            System.out.print("[CHAT] Nickname: ");
-            str = stdIn.readLine();
-            MakeDir(str);
-            user = new User(str,port,LOCALHOST,KEYDIRECTORY);
+            ch = menu.InitialMenu();
+            /* Registration */
+            if(ch == 0) {
+                while(true){
+                    cred = menu.NewUserMenu();
+                    if(cred[1].length()<2){
+                        System.out.println("Warning: Password is too short.");
+                        //mettere una sleep
+                        continue;
+                    }
+                    if(!login.newUser(cred[0],cred[1])){
+                        System.out.println("Warning: User name already in use.");
+                        //mettere una sleep
+                    }
+                    else break;
+                }                              
+            }
+
+            /* Login */
+            if(ch == 1) {
+                cred = menu.RegisteredMenu();
+                if(!login.LoadUser(cred[0],cred[1])){
+                    System.out.println("Error: User or Password are invalid.");
+                    System.exit(0);
+                }
+
+            }
+                        
             
-            //Sets rsa parameters
-            /*Checks whether file keys already exists, in other case has created*/
+            /*Checks whether the user directory exists*/
+            MakeDir(cred[0]);
+            /*Loads user credential*/
+            user = new User(cred[0],port,LOCALHOST,KEYDIRECTORY);
+            
+            /*Sets rsa parameters.
+            Checks whether file keys already exists, in other case has created*/
             user.CreateRsa();
             
+            /*Shows chat board, showing the port given*/
+            menu.ChatBoard("Listening port: " + Integer.toString(port));
+
             ct = new ClientThread (user,true);
             ct.start();
             
@@ -49,6 +85,7 @@ public class Client {
                     if (!ct.IsConnected())continue;
                     ct.setAccepted(true);
                     ct.signalConnected();
+                    menu.ChatBoard("");
                     break;
                 }
                 if(str.compareTo("n")==0){
---
+++ SecureLogin.java
@@ -0,0 +1,182 @@
+import java.security.SecureRandom;
+import java.util.Random;
+import java.io.*;
+import java.net.*;
+import java.lang.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Hashtable;
+import java.util.Enumeration;
+
+public class SecureLogin{
+    static final int ITERATION = 1000;
+    static final String CREDENTIALSPATH = "/home/badnack/Projects/SecureChat/Ssl-Chat/Credentials/";
+    static final String FILENAME = "passwd";
+    
+    /**Retrieves a string from a bytes array
+       @param arr: array of bytes
+       @return String: Converted byte
+       @throws UnsupportedEncodingException*/
+    private String getText (byte[] arr) throws UnsupportedEncodingException
+    {
+        String s = new String( arr, "UTF-8" );
+        return s;
+    }
+
+    /**Computes Salt
+       @return byte[]: salt bytes*/
+    private byte[] getSalt(){
+        Random r = new SecureRandom();
+        byte[] salt = new byte[20];
+        r.nextBytes(salt);
+        return salt;
+    }
+
+    /**Computes hash from a string, using SHA1
+     @param password: password whereby has calculated
+     @param salt: salt bytes
+     @return byte[]: Hash code
+     @throws NoSuchAlgorithmException
+     @throws UnsupportedEncodingException*/
+    private byte[] getHash(String password, byte[] salt) throws NoSuchAlgorithmException,UnsupportedEncodingException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        digest.reset();
+        digest.update(salt);
+        byte[] input = digest.digest(password.getBytes("UTF-8"));
+        for (int i = 0; i < ITERATION; i++) {
+            digest.reset();
+            input = digest.digest(input);
+        }
+        return input;
+   }
+
+    /** Checks if a given user name has already taken;
+        @param user: user name
+        @return boolean true whether the given name is free, false otherwise
+        @throws IOException
+        @thorws FileNotFoundException
+    */
+    private boolean CheckUserName(String UserName) throws IOException,FileNotFoundException{
+        int ln,ls,lh;
+        Directory.MakeDirectory(CREDENTIALSPATH);
+        try{
+            FileInputStream fis = new FileInputStream(new File(CREDENTIALSPATH + FILENAME));
+            fis.close();
+        }catch(FileNotFoundException x){return true;}
+        
+        FileInputStream fis = new FileInputStream(new File(CREDENTIALSPATH + FILENAME));
+        
+        try{
+            while(true){                 
+                /*Reads length values*/
+                ln = (int) fis.read();
+                fis.skip(1);
+                ls = (int) fis.read();
+                fis.skip(1);
+                lh = (int) fis.read();
+                fis.skip(1);               
+                //Reads user name
+                byte[] apn = new byte[ln];
+                fis.read(apn);                
+                //Checks user name
+                if(UserName.compareTo(getText(apn))!=0){
+                    fis.skip((int)(ls+lh+1));
+                    continue; }               
+                else return false;                                
+                }                
+        }
+        catch(Exception x){fis.close();return true;}
+    
+    }
+    /** Puts a new user in the users list
+     @param UserName: user name
+     @param password: password
+     @return boolean: true whether everything has gone well, false otherwise
+     @throws IOException
+     @throws FileNotFoundException
+     @throws NoSuchAlgorithmException
+     @throws UnsupportedEncodingException*/
+    public boolean newUser(String UserName, String password) throws IOException,FileNotFoundException,NoSuchAlgorithmException,UnsupportedEncodingException{
+        
+        if(!CheckUserName(UserName))return false;        
+        byte[] salt = getSalt();       
+        byte[] hash = getHash(password,salt);
+        FileOutputStream fos = new FileOutputStream(CREDENTIALSPATH + FILENAME,true);                    
+
+        //save file
+         int ls,lh,ln;
+
+        ln = UserName.length();
+        lh = hash.length;
+        ls = salt.length;
+        
+        fos.write((int)ln);
+        fos.write('\n');
+        fos.write((int)ls);
+        fos.write('\n');
+        fos.write((int)lh);
+        fos.write('\n');
+        
+        
+        fos.write(UserName.getBytes());
+        fos.write(salt);
+        fos.write(hash);
+        fos.write('\n');
+        fos.close();          
+        return true;    
+    }
+    
+    
+    /**Checks if user credentials are valids
+     @param user: user name
+     @param password: password
+     @return boolean: true whether the credentials given are correct, false otherwise
+     @throws IOException
+     @throws FileNotFoundException*/
+    public boolean LoadUser(String UserName, String password)  throws IOException,FileNotFoundException{
+        int ln,ls,lh;
+        Directory.MakeDirectory(CREDENTIALSPATH);
+        try{
+            FileInputStream fis = new FileInputStream(new File(CREDENTIALSPATH + FILENAME));
+            fis.close();
+        }catch(FileNotFoundException x){return false;}
+        
+        FileInputStream fis = new FileInputStream(new File(CREDENTIALSPATH + FILENAME));
+        
+        try{
+             while(true){                 
+                /*Reads length values*/
+                ln = (int) fis.read();
+                fis.skip(1);
+                ls = (int) fis.read();
+                fis.skip(1);
+                lh = (int) fis.read();
+                fis.skip(1);
+                //Reads user name
+                byte[] apn = new byte[ln];
+                fis.read(apn);
+                
+                //Checks user name
+                if(UserName.compareTo(getText(apn))!=0){
+                    fis.skip((int)(ls+lh+1));
+                    continue;
+                }    
+           
+                else{
+                    byte[] aps = new byte[ls]; //gets salt
+                    fis.read(aps);
+                    byte[] aph = new byte[lh]; //gets hash
+                    fis.read(aph);
+                    byte[] chpwd = getHash(password,aps); //compute hash                                       
+                    if((getText(aph)).compareTo( getText(chpwd))==0) return true;
+                    else return false;                                        
+                }
+                
+             }
+             
+        }
+        catch(Exception x){fis.close();return false;}
+        
+    }
+             
+}
